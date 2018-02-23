d4b53946a1c9c328baca67dfd59d0cf94b524ab8
J.D. Wagenveld <s1467603@leidenuniv.nl>
2018-02-23 15:52:27 +0100

    Task 3 done(ish), started on task 4

--- assignment1.py
+++ assignment1.py
@@ -37,22 +37,8 @@ def plot_confusion_matrix(cm, classes, ax,
     ax.set_xlabel('Predicted label')
     ax.set_xlim(0,9)
     ax.set_ylim(9,0)
-
-def custom_euclidean(x_means, train_x):
-    dist_points = np.zeros((10,len(train_x)))
-
-    for j in range(len(train_x)):
-        for i in range(10):
-            dist_points[i,j] = np.linalg.norm(train_x[j,:] - x_means[:,i])
-    return dist_points
-
-def main():
-    train_x = np.genfromtxt('data/train_in.csv', delimiter=',')
-    train_y = np.genfromtxt('data/train_out.csv', delimiter=',')
-
-    test_x = np.genfromtxt('data/test_in.csv', delimiter=',')
-    test_y = np.genfromtxt('data/test_out.csv', delimiter=',')
-
+    
+def task1_2(train_x, train_y, test_x, test_y):
     x_means = np.zeros((10,256))
     radius = np.zeros((10))
 
@@ -75,22 +61,107 @@ def main():
             dist_clouds[i,j] = np.linalg.norm(x_means[:,i]-x_means[:,j])
     print dist_clouds
 
-    dist_points = np.zeros((len(train_x),10))
+    dist_points = np.zeros((len(test_x),10))
 
     fig, axs = plt.subplots(2,3, figsize=(20,15), sharex=True, sharey=True)
     ax = axs.ravel()
 
     for j, metric in enumerate(['cityblock', 'cosine', 'euclidean', 'l1', 'l2', 'manhattan']):
-    	for i in range(len(train_x)):
-    		dist_points[i,:] = metrics.pairwise.pairwise_distances(train_x[i,:].reshape(1,-1),x_means, metric=metric)[0]
-    	closest = np.argmin(dist_points, axis=1)
-
-    	confusion = metrics.confusion_matrix(train_y, closest)
-
-    	plot_confusion_matrix(confusion, classes=['0','1','2','3','4','5','6','7','8','9'], ax=ax[j], title=metric, normalize=True)
-    	print 'Accuracy of {0} : {1}%'.format(metric,np.trace(confusion)*100./confusion.sum())
+      for i in range(len(test_x)):
+    	   dist_points[i,:] = metrics.pairwise.pairwise_distances(test_x[i,:].reshape(1,-1),
+    	                                                          x_means, metric=metric)[0]
+      closest = np.argmin(dist_points, axis=1)
+      confusion = metrics.confusion_matrix(test_y, closest)
+      plot_confusion_matrix(confusion, classes=['0','1','2','3','4','5','6','7','8','9'], 
+                            ax=ax[j], title=metric, normalize=True)
+      print 'Accuracy of {0} : {1}%'.format(metric,np.trace(confusion)*100./confusion.sum())
+    
     plt.tight_layout()
     plt.show()
+    
+def task3(train_x, train_y, test_x, test_y):
+    fives_train = train_x[train_y == 5,:]
+    threes_train = train_x[train_y == 1,:]
+        
+    five_ratio_train = calc_ratio(fives_train)  
+    three_ratio_train = calc_ratio(threes_train)
+    
+    n_five, bins_five, patches = plt.hist(five_ratio_train, bins=20, alpha=0.5, normed=True)
+    n_three, bins_three, patches = plt.hist(three_ratio_train[three_ratio_train < 10], bins=20, 														  alpha=0.5, normed=True)
+    plt.show()
+    
+    fives = test_x[test_y == 5,:]
+    threes = test_x[test_y == 1,:]
+    
+    print 'Number of 5s: ', len(fives)
+    print 'Number of 3s: ', len(threes)
+    
+    prior_three = float(len(threes_train))/(len(fives_train)+len(threes_train))
+    prior_five = float(len(fives_train))/(len(fives_train)+len(threes_train))
+    
+    five_ratio = calc_ratio(fives)  
+    three_ratio = calc_ratio(threes)
+    
+    all_n = np.concatenate((five_ratio,three_ratio))
+    
+    binned_five = np.digitize(all_n, bins_five)
+    binned_five[binned_five >= len(n_five)] = len(n_five)-1
+    binned_three = np.digitize(all_n, bins_three)
+    binned_three[binned_three >= len(n_three)] = len(n_three)-1
+    
+    classified_five = np.zeros(len(all_n))
+    for i in range(len(all_n)):
+         if (n_five[binned_five[i]]*prior_five > n_three[binned_three[i]]*prior_three):
+            classified_five[i] = 1
+      
+    accuracy = (np.sum(1*classified_five[:len(fives)]==1)+
+    				 np.sum(1*classified_five[len(fives):]==0))/float(len(classified_five))
+    print 'Accuracy : {}%'.format(accuracy*100)
+
+def custom_euclidean(x_means, train_x):
+    dist_points = np.zeros((10,len(train_x)))
+
+    for j in range(len(train_x)):
+        for i in range(10):
+            dist_points[i,j] = np.linalg.norm(train_x[j,:] - x_means[:,i])
+    return dist_points
+    
+def calc_ratio(number):
+    number_ratio = np.zeros(len(number))
+    ones_number = 1*(number==1)
+    for i in range(len(number)):
+      temp = ones_number[i,:].reshape(16,16)
+      number_ratio[i] = np.sum(temp[:,:8])/float(np.sum(temp[:,8:]))
+    return number_ratio
+   
+def g_step(X):
+	for i in range(len(X)):
+		if X[i] < 0:
+			X[i] = -1
+		if X[i] => 0:
+			X[i] = 1
+	return X 
+
+def main():
+	train_x = np.genfromtxt('data/train_in.csv', delimiter=',')
+	train_y = np.genfromtxt('data/train_out.csv', delimiter=',')
+
+	test_x = np.genfromtxt('data/test_in.csv', delimiter=',')
+	test_y = np.genfromtxt('data/test_out.csv', delimiter=',')
+
+	#task1_2(train_x, train_y, test_x, test_y)
+	#task3(train_x, train_y, test_x, test_y)
+
+	w = np.random.randn(257,10)
+	w[0,:] = 1
+	g = np.zeros((len(train_x),10))
+	
+	while accuracy < 1:
+		for i in range(10):
+			a = np.dot(w[1:,i],train_x)+w[0,:]
+			g[:,i] = g_step(a)
+			
+
 
 if __name__ == '__main__':
     main()
