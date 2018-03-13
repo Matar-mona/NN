51a5209e9b356ef2aaf0ce292bbd1e5529de99ea
Jonah Wagenveld <wagenveld@meije.strw.leidenuniv.nl>
2018-03-13 11:25:54 +0100

    Some minor changes

--- assignment1.py
+++ assignment1.py
@@ -2,9 +2,22 @@ from sklearn import metrics
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
+import cPickle
 import itertools
 import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
+
+def pickle_to_file(data, fname):
+	fh = open(fname, 'w') 
+	cPickle.dump(data, fh) 
+	fh.close() 
+
+def pickle_from_file(fname): 
+    fh = open(fname, 'r') 
+    data = cPickle.load(fh) 
+    fh.close() 
+    return data
 
 def custom_euclidean(x_means, train_x):
     dist_points = np.zeros((10,len(train_x)))
@@ -51,8 +64,10 @@ def plot_confusion_matrix(cm, classes, ax,
     ax.imshow(cm, interpolation='nearest', cmap=cmap)
     ax.set_title(title)
     tick_marks = np.arange(len(classes))
-    ax.set_xticks(tick_marks, classes)
-    ax.set_yticks(tick_marks, classes)
+    ax.set_xticks(tick_marks)
+    ax.set_xticklabels(classes)
+    ax.set_yticks(tick_marks)
+    ax.set_yticklabels(classes)
 
     fmt = '{:.0f}%' if normalize else 'd'
     thresh = cm.max() / 2.
@@ -61,8 +76,6 @@ def plot_confusion_matrix(cm, classes, ax,
                  horizontalalignment="center",
                  color="white" if cm[i, j] > thresh else "black")
 
-    ax.set_ylabel('True label')
-    ax.set_xlabel('Predicted label')
     ax.set_xlim(0,9)
     ax.set_ylim(9,0)
     
@@ -87,11 +100,12 @@ def task1_2(train_x, train_y, test_x, test_y):
     for i in range(10):
         for j in range(10):
             dist_clouds[i,j] = np.linalg.norm(x_means[:,i]-x_means[:,j])
+    print 'Distance between clouds:'
     print dist_clouds
 
     dist_points = np.zeros((len(test_x),10))
 
-    fig, axs = plt.subplots(2,3, figsize=(20,15), sharex=True, sharey=True)
+    fig, axs = plt.subplots(2,3, figsize=(14,9.2), sharex=True, sharey=True)
     ax = axs.ravel()
 
     for j, metric in enumerate(['cityblock', 'cosine', 'euclidean', 'l1', 'l2', 'manhattan']):
@@ -105,6 +119,7 @@ def task1_2(train_x, train_y, test_x, test_y):
       print 'Accuracy of {0} : {1}%'.format(metric,np.trace(confusion)*100./confusion.sum())
     
     plt.tight_layout()
+    plt.savefig('confusion_matrices.png', dpi=300)
     plt.show()
     
 def task3(train_x, train_y, test_x, test_y):
@@ -169,7 +184,10 @@ def task4(train_x, train_y, test_x, test_y):
 	acctrain = 1-np.sum(missclassified)/float(len(missclassified))
 	print 'Accuracy on training set after {1} iterations: {0}%'.format(acctrain*100,iterations)
 	
-	plt.plot(np.arange(iterations), acc)
+	plt.plot(np.arange(iterations), acc, color='k')
+	plt.ylabel('Accuracy')
+	plt.xlabel('Iteration')
+	plt.savefig('task4.png', dpi=300)
 	plt.show()
 	
 	a = np.dot(w[:,1:],test_x.T)+w[:,0].reshape(-1,1)
@@ -194,7 +212,7 @@ def task5(train_x, train_y, test_x, test_y):
 		for i in range(4):
 			xor = xor_net(x[i,0],x[i,1],weights)
 			mse += (xor-y[i])**2
-		return mse
+		return mse/4.
 	
 	def grdmse(weights):
 		err = 0.001
@@ -226,8 +244,80 @@ def task5(train_x, train_y, test_x, test_y):
 		print xor_net(x[i,0],x[i,1],w)
 	print 'Mean squared error after {0} iterations: {1}'.format(iterations,error)
 	
-	plt.plot(np.arange(iterations),mses)
+	plt.plot(np.arange(iterations),mses, color='k')
+	plt.ylabel('Mean squared error')
+	plt.xlabel('Iteration')
+	plt.savefig('task5.png', dpi=300)
 	plt.show()
+
+def task5_opt(train_x, train_y, test_x, test_y, Y_train):
+
+	def mnist_net(X, weights1, weights2):
+		a1 = sigmoid(np.dot(weights1[:,1:],X.T)+weights1[:,0].reshape(-1,1))
+		a2 = sigmoid(np.dot(weights2[:,1:],a1)+weights2[:,0].reshape(-1,1))
+		return a2, a2.argmax(axis=0)
+	
+	def mse(weights1,weights2):
+		mse = 0
+		A2, y_hat = mnist_net(train_x,weights1,weights2)
+		mse = np.sum((A2-Y_train)**2)
+		return mse/float(len(train_y))
+	
+	def grdmse(weights1, weights2):
+		err = 0.001
+		grads1 = np.zeros((30,257))
+		grads2 = np.zeros((10,31))
+		weights_err1 = np.copy(weights1)
+		weights_err2 = np.copy(weights2)
+		const_mse = mse(weights1,weights2)
+		for i in range(10):
+			for j in range(31):
+				weights_err2[i,j] += err
+				grads2[i,j] = (mse(weights1,weights_err2)-const_mse)/err
+				weights_err2[i,j] -= err
+		for i in range(30):
+			for j in range(257):
+				weights_err1[i,j] += err
+				grads1[i,j] = (mse(weights_err1,weights2)-const_mse)/err
+				weights_err1[i,j] -= err
+		return grads1, grads2
+			
+	w1 = np.random.randn(30,257)*0.5
+	w1[:,0] = 1
+	w2 = np.random.randn(10,31)*0.5
+	w2[:,0] = 1
+	learning_rate = 5
+	
+	error = 1
+	iterations = 0
+	mses = []                                     
+	while error > 0.01:
+		grads1, grads2 = grdmse(w1,w2)
+		w1 -= learning_rate*grads1
+		w2 -= learning_rate*grads2
+		error = mse(w1,w2)
+		mses.append(error)
+		iterations += 1
+		print 'Mean squared error at iteration {0}: {1}'.format(iterations,error)
+		if iterations > 1000:
+			break 
+
+	pickle_to_file((w1,w2), 'weights.pkl')		
+
+	print 'Mean squared error after {0} iterations: {1}'.format(iterations,error)
+	
+	plt.plot(np.arange(iterations),mses, color='k')
+	plt.ylabel('Mean squared error')
+	plt.xlabel('Iteration')
+	plt.savefig('opt_task5.png', dpi=300)
+	plt.close()
+
+	a1 = sigmoid(np.dot(w1[:,1:],X.T)+w1[:,0].reshape(-1,1))
+	a2 = sigmoid(np.dot(w2[:,1:],a1)+w2[:,0].reshape(-1,1))
+	y_h = a2.argmax(axis=0)
+	missclassified = 1*np.not_equal(y_h,test_y)
+	acctest = 1-np.sum(missclassified)/float(len(missclassified))
+	print 'Accuracy on test set: {0}%'.format(acctest*100)
 
 def main():
 	train_x = np.genfromtxt('data/train_in.csv', delimiter=',')
@@ -236,64 +326,15 @@ def main():
 	test_x = np.genfromtxt('data/test_in.csv', delimiter=',')
 	test_y = np.genfromtxt('data/test_out.csv', delimiter=',')
 
+	y_matrix = np.zeros((10,len(train_y)))
+	for i in range(len(train_y)):
+		y_matrix[int(train_y[i]),i] = 1 
+
 	#task1_2(train_x, train_y, test_x, test_y)
 	#task3(train_x, train_y, test_x, test_y)
 	#task4(train_x, train_y, test_x, test_y)
 	#task5(train_x, train_y, test_x, test_y)
-	
-	def mnist_net(X, weights1, weights2):
-		a1 = np.tanh(np.dot(weights1[:,1:],X.T)+weights1[:,0].reshape(-1,1))
-		a2 = np.tanh(np.dot(weights2[:,1:],a1)+weights2[:,0].reshape(-1,1))
-		return a2.argmax(axis=0)
-	
-	def mse(weights1,weights2):
-		mse = 0
-		y_hat = mnist_net(train_x,weights1,weights2)
-		mse = np.sum((y_hat-train_y)**2)
-		return mse
-	
-	def grdmse(weights1, weights2):
-		err = 0.001
-		grads1 = np.zeros((30,257))
-		grads2 = np.zeros((10,31))
-		weights_err1 = np.copy(weights1)
-		weights_err2 = np.copy(weights2)
-		for i in range(10):
-			for j in range(31):
-				weights_err2[i,j] += err
-				grads2[i,j] = (mse(weights1,weights_err2)-mse(weights1,weights2))/err
-				weights_err2[i,j] -= err
-		for i in range(30):
-			for j in range(257):
-				weights_err1[i,j] += err
-				grads1[i,j] = (mse(weights_err1,weights2)-mse(weights1,weights2))/err
-				weights_err1[i,j] -= err
-		return grads1, grads2
-			
-	w1 = np.random.randn(30,257)*0.5
-	w1[:,0] = 1
-	w2 = np.random.randn(10,31)*0.5
-	w2[:,0] = 1
-	learning_rate = 0.1
-	
-	error = 1
-	iterations = 0
-	mses = []                                     
-	while error > 0.01:
-		print iterations
-		grads1, grads2 = grdmse(w1,w2)
-		w1 -= learning_rate*grads1
-		w2 -= learning_rate*grads2
-		error = mse(w1,w2)
-		mses.append(error)
-		iterations += 1
-		if iterations > 10:
-			break 
+	task5_opt(train_x, train_y, test_x, test_y, y_matrix)
 
-	print 'Mean squared error after {0} iterations: {1}'.format(iterations,error)
-	
-	plt.plot(np.arange(iterations),mses)
-	plt.show()
-		
 if __name__ == '__main__':
     main()
