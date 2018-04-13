16a999ae28aa086fe73f597f8447e3ca70dcaf55
J.D. Wagenveld <s1467603@leidenuniv.nl>
2018-04-13 12:15:00 +0200

    Frankensteins monster

--- Assignment_2/vae_autoencoder_imdb.py
+++ Assignment_2/vae_autoencoder_imdb.py
@@ -1,76 +1,63 @@
-from keras.callbacks import ModelCheckpoint
-from keras.datasets import imdb
-from keras.preprocessing.sequence import pad_sequences
-from model import VAE
+from __future__ import print_function
+
 import numpy as np
-import os
+import matplotlib.pyplot as plt
+from scipy.stats import norm
 
-class VAE(object):
-    def create(self, vocab_size=500, max_length=300, latent_rep_size=200):
-        self.encoder = None
-        self.decoder = None
-        self.sentiment_predictor = None
-        self.autoencoder = None
-
-        x = Input(shape=(max_length,))
-        x_embed = Embedding(vocab_size, 64, input_length=max_length)(x)
-
-        vae_loss, encoded = self._build_encoder(x_embed, latent_rep_size=latent_rep_size, max_length=max_length)
-        self.encoder = Model(inputs=x, outputs=encoded)
-
-        encoded_input = Input(shape=(latent_rep_size,))
-        predicted_sentiment = self._build_sentiment_predictor(encoded_input)
-        self.sentiment_predictor = Model(encoded_input, predicted_sentiment)
-
-        decoded = self._build_decoder(encoded_input, vocab_size, max_length)
-        self.decoder = Model(encoded_input, decoded)
-
-        self.autoencoder = Model(inputs=x, outputs=[self._build_decoder(encoded, vocab_size, max_length), self._build_sentiment_predictor(encoded)])
-        self.autoencoder.compile(optimizer='Adam',
-                                 loss=[vae_loss, 'binary_crossentropy'],
-                                 metrics=['accuracy'])
-
-	def _build_encoder(self, x, latent_rep_size=200, max_length=300, epsilon_std=0.01):
-		    h = Bidirectional(LSTM(500, return_sequences=True, name='lstm_1'), merge_mode='concat')(x)
-		    h = Bidirectional(LSTM(500, return_sequences=False, name='lstm_2'), merge_mode='concat')(h)
-		    h = Dense(435, activation='relu', name='dense_1')(h)
-
-	    def sampling(args):
-	        z_mean_, z_log_var_ = args
-	        batch_size = K.shape(z_mean_)[0]
-	        epsilon = K.random_normal(shape=(batch_size, latent_rep_size), mean=0., stddev=epsilon_std)
-	        return z_mean_ + K.exp(z_log_var_ / 2) * epsilon
-
-	    z_mean = Dense(latent_rep_size, name='z_mean', activation='linear')(h)
-	    z_log_var = Dense(latent_rep_size, name='z_log_var', activation='linear')(h)
-
-	    def vae_loss(x, x_decoded_mean):
-	        x = K.flatten(x)
-	        x_decoded_mean = K.flatten(x_decoded_mean)
-	        xent_loss = max_length * objectives.binary_crossentropy(x, x_decoded_mean)
-	        kl_loss = - 0.5 * K.mean(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
-	        return xent_loss + kl_loss
-
-	    return (vae_loss, Lambda(sampling, output_shape=(latent_rep_size,), name='lambda')([z_mean, z_log_var]))
-
-    def _build_decoder(self, encoded, vocab_size, max_length):
-	    repeated_context = RepeatVector(max_length)(encoded)
-
-	    h = LSTM(500, return_sequences=True, name='dec_lstm_1')(repeated_context)
-	    h = LSTM(500, return_sequences=True, name='dec_lstm_2')(h)
-
-	    decoded = TimeDistributed(Dense(vocab_size, activation='softmax'), name='decoded_mean')(h)
-
-	    return decoded
-
-	def _build_sentiment_predictor(self, encoded):
-	    h = Dense(100, activation='linear')(encoded)
-
-	    return Dense(1, activation='sigmoid', name='pred')(h)
+from keras.layers import Input, Dense, Lambda
+from keras.models import Model
+from keras import backend as K
+from keras import metrics
+from keras.datasets import imdb
 
 MAX_LENGTH = 300
 NUM_WORDS = 1000
+vocab_size = 500
+latent_rep_size = 200
 
+x = Input(shape=(MAX_LENGTH,))
+x_embed = Embedding(vocab_size, 64, input_length=MAX_LENGTH)(x)
+encoder_h = Bidirectional(LSTM(500, return_sequences=True, name='lstm_1'), merge_mode='concat')(x)
+encoder_h = Bidirectional(LSTM(500, return_sequences=False, name='lstm_2'), merge_mode='concat')(encoder_h)
+encoder_h = Dense(435, activation='relu', name='dense_1')(encoder_h)
+z_mean = Dense(latent_rep_size, name='z_mean', activation='linear')(encoder_h)
+z_log_var = Dense(latent_rep_size, name='z_log_var', activation='linear')(encoder_h)
+
+def sampling(args):
+    z_mean, z_log_var = args
+    epsilon = K.random_normal(shape=(K.shape(z_mean)[0], latent_dim), mean=0.,
+                              stddev=epsilon_std)
+return z_mean + K.exp(z_log_var / 2) * epsilon
+
+def _build_sentiment_predictor(encoded):
+    h = Dense(100, activation='linear')(encoded)
+
+    return Dense(1, activation='sigmoid', name='pred')(h)
+
+encoded_input = Input(shape=(latent_rep_size,))
+predicted_sentiment = _build_sentiment_predictor(encoded_input)
+sentiment_predictor = Model(encoded_input, predicted_sentiment)
+
+repeated_context = RepeatVector(MAX_LENGTH)(encoded)
+decoder_h = LSTM(500, return_sequences=True, name='dec_lstm_1')(repeated_context)
+decoder_h = LSTM(500, return_sequences=True, name='dec_lstm_2')(decoder_h)
+h_decoded = TimeDistributed(Dense(vocab_size, activation='softmax'), name='decoded_mean')(decoder_h)
+x_decoded_mean = decoder_mean(h_decoded)
+decoder = Model(encoded_input, h_decoded)
+
+# Compute VAE loss
+x = K.flatten(x)
+x_decoded_mean = K.flatten(x_decoded_mean)
+xent_loss = max_length * objectives.binary_crossentropy(x, x_decoded_mean)
+kl_loss = - 0.5 * K.mean(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
+vae_loss = xent_loss + kl_loss
+
+autoencoder = Model(x, x_decoded_mean)
+autoencoder.compile(optimizer='Adam',
+                         loss=[vae_loss, 'binary_crossentropy'],
+                         metrics=['accuracy'])
+
+#IMDB dataset
 (X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=NUM_WORDS)
 
 print("Training data")
