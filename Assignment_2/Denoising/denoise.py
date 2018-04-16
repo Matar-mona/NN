e4c2317c54d3fcdf1c541442ac3b00ddfff5da4e
J.D. Wagenveld <s1467603@duranium.liacs.nl>
2018-04-16 11:31:54 +0200

    Denoising

--- Assignment_2/Denoising/denoise.py
+++ Assignment_2/Denoising/denoise.py
@@ -10,6 +10,8 @@ from pathlib import Path
 import glob
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib
+matplotlib.use('agg')
 import matplotlib.pyplot as plt
 from skimage.io import imread, imshow, imsave
 from keras.preprocessing.image import load_img, array_to_img, img_to_array
@@ -20,27 +22,18 @@ from keras import backend as K
 from sklearn.model_selection import train_test_split
 np.random.seed(111)
 # Input data files are available in the "../input/" directory.
-# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
 
-from subprocess import check_output
-print(check_output(["ls", "../input"]).decode("utf8"))
-
-input_dir  = Path('C:/Users/mmata/Desktop/NN/Denoising')
-train = input_dir / 'train'
-train_cleaned = input_dir / 'train_cleaned'
-test = input_dir / 'test'
-
-train_images = sorted(os.listdir(train))
-#train_labels = sorted(os.listdir(train_cleaned))
-test_images = sorted(os.listdir(test))
+train_images = sorted(os.listdir('train'))
+train_labels = sorted(os.listdir('train_cleaned'))
+test_images = sorted(os.listdir('test'))
 print("Total number of images in the training set: ", len(train_images))
 print("Total number of cleaned images found: ", len(train_labels))
 print("Total number of samples in the test set: ", len(test_images))
 
 # The train directory comtaims png files. Let's get all the files and check a few samples
-train_images = sorted(os.listdir(train))
-train_labels = sorted(os.listdir(train_cleaned))
-test_images = sorted(os.listdir(test))
+train_images = sorted(os.listdir('train'))
+train_labels = sorted(os.listdir('train_cleaned'))
+test_images = sorted(os.listdir('test'))
 print("Total number of images in the training set: ", len(train_images))
 print("Total number of cleaned images found: ", len(train_labels))
 print("Total number of samples in the test set: ", len(test_images))
@@ -51,13 +44,14 @@ samples = train_images[:3] + train_labels[:3]
 
 f, ax = plt.subplots(2, 3, figsize=(20,10))
 for i, img in enumerate(samples):
-    img = imread(train/img)
+    img = imread('train/'+str(img))
     ax[i//3, i%3].imshow(img, cmap='gray')
     ax[i//3, i%3].axis('off')
-plt.show()   
+plt.savefig('denoise_samples.png',dpi=300)
+plt.close()   
 
 # Lets' define our autoencoder now
-def build_autoenocder():
+def build_autoencoder():
     input_img = Input(shape=(420,540,1), name='image_input')
     
     #enoder 
@@ -78,19 +72,19 @@ def build_autoenocder():
     autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
     return autoencoder
 
-autoencoder = build_autoenocder()
+autoencoder = build_autoencoder()
 autoencoder.summary()
 
 X = []
 Y = []
 
 for img in train_images:
-    img = load_img(train / img, grayscale=True,target_size=(420,540))
+    img = load_img('train/'+str(img), grayscale=True,target_size=(420,540))
     img = img_to_array(img).astype('float32')/255.
     X.append(img)
 
 for img in train_labels:
-    img = load_img(train_cleaned / img, grayscale=True,target_size=(420,540))
+    img = load_img('train_cleaned/'+str(img), grayscale=True,target_size=(420,540))
     img = img_to_array(img).astype('float32')/255.
     Y.append(img)
 
@@ -110,7 +104,7 @@ print("Total number of validation samples: ", X_valid.shape)
 autoencoder.fit(X_train, y_train, epochs=10, batch_size=8, validation_data=(X_valid, y_valid))
 
 # Let's test our model on a sample test image
-sample_test = load_img(test/ test_images[10], grayscale=True, target_size=(420,540))
+sample_test = load_img('test/'+str(test_images[10]), grayscale=True, target_size=(420,540))
 sample_test = img_to_array(sample_test)
 sample_test_img = sample_test.astype('float32')/255.
 sample_test_img = np.expand_dims(sample_test, axis=0)
@@ -121,4 +115,5 @@ predicted_label = np.squeeze(autoencoder.predict(sample_test_img))
 f, ax = plt.subplots(1,2, figsize=(10,8))
 ax[0].imshow(np.squeeze(sample_test), cmap='gray')
 ax[1].imshow(np.squeeze(predicted_label.astype('int8')), cmap='gray')
-plt.show()  
+plt.savefig('denoise_auto.png', dpi=300)
+plt.close()  
