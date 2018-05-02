b84141dde82b1ea41ce67add58d3d315be26069c
J.D. Wagenveld <s1467603@leidenuniv.nl>
2018-05-02 12:59:41 +0200

    Basic architecture

--- Assignment_3/colorize_cnn.py
+++ Assignment_3/colorize_cnn.py
@@ -1,11 +1,12 @@
-from PIL import Image
+from PIL import Image, ImageCms
+from skimage.color import rgb2lab, lab2rgb 
 import numpy as np
 import glob
 import math
 from time import time
 
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Dense, Dropout, Flatten, ZeroPadding2D
 from keras.layers import Conv2D, MaxPooling2D
 from sklearn.model_selection import train_test_split
 
@@ -27,13 +28,28 @@ def load_data(image_size):
 		image = image.crop(box=square_box)
 		img = image.resize(size=(image_size,image_size))
 
-		X[i,:,:,:] = np.array(img.convert('L'))[:,:,np.newaxis]
-		Y[i,:,:,:] = np.array(img)
+		img_arr = np.array(img)/255.
+		X[i,:,:,:] = rgb2lab(img_arr)[:,:,0]
+		Y[i,:,:,:] = rgb2lab(img_arr)[:,:,1:]
 		image.close()
+	Y /= 128.
 	return X, Y
 
 def initialize_model(image_size=imsize):
+
 	model = Sequential()
+	model.add(ZeroPadding2D(1,1), input_shape=(imsize,imsize,1))
+	model.add(Conv2D(16, (5,5), strides=3, activation='relu'))
+	model.add(ZeroPadding2D((1,1)))
+	model.add(Conv2D(32, (5,5), strides=3, activation='relu'))
+	model.add(ZeroPadding2D((1,1)))
+	model.add(Conv2D(64, (5,5), strides=3, activation='relu'))
+	model.add(UpSampling2D((2, 2)))
+	model.add(ZeroPadding2D((1,1)))
+	model.add(Conv2D(32, (5,5), strides=3, activation='relu'))
+	model.add(UpSampling2D((2, 2)))
+	model.add(ZeroPadding2D((1,1)))
+	model.add(Conv2D(16, (5,5), strides=3, activation='relu'))
 
 def main():
 	start = time()
