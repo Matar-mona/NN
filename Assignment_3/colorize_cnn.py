f3c7f437b7f9730a0602935434d5ca680ad2f145
Jonah Wagenveld <wagenveld@student49.strw.leidenuniv.nl>
2018-05-16 20:41:34 +0200

    Final commit Assignment 3

--- Assignment_3/colorize_cnn.py
+++ Assignment_3/colorize_cnn.py
@@ -8,7 +8,7 @@ import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
 
-from keras.callbacks import Callback
+from keras.callbacks import Callback, TensorBoard
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, BatchNormalization
 from keras.layers import Conv2D, MaxPooling2D, UpSampling2D
@@ -34,7 +34,15 @@ class OutputObserver(Callback):
 			show_image(self.x_train[0], self.y_train[0], prediction[0], Save=True, epoch=epoch)
 
 def show_image(L_image, ab_image, ab_image_pred, Save=False, epoch=None):
+	"""
+	Show an image containing input image, ground truth, prediction and a histogram of ab-values of the prediction
 
+	L_image: Input black and white image
+	ab_image: True ab-values of the image
+	ab_image_pred: Predicted ab-values of the image
+	Save: Whether to save the image
+	epoch: Current epoch
+	"""
 	prediction = np.zeros((256,256,3))
 	prediction[:,:,0] = L_image[:,:,0]
 	prediction[:,:,1:] = ab_image_pred*128
@@ -75,16 +83,27 @@ def show_image(L_image, ab_image, ab_image_pred, Save=False, epoch=None):
 
 
 def load_data(path):
+	"""
+	Load in the data from from the file and split the data into a training and a test set
+
+	path: Path to .h5 file containing all the data
+	"""
 	print('Loading in data...\n')
 	with h5py.File(path,'r') as hf:
-		X = hf['X'][:]
-		Y = hf['Y'][:]
+		X = hf['X'][:50]
+		Y = hf['Y'][:50]
 
 	X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25, random_state=42)
 
 	return X_train, X_test, Y_train, Y_test
 
 def initialize_model(kernel_size, learning_rate):
+	"""
+	Initialize the neural network and create the model
+
+	kernel_size: Kernel size of the convolutional layers
+	learning_rate: Learning rate of the network
+	"""
 
 	model = Sequential()
 
@@ -116,6 +135,7 @@ def initialize_model(kernel_size, learning_rate):
 
 	model.add(Conv2D(2, kernel_size, padding='same', activation='tanh'))
 
+	#Specify optimizer, learning rate and loss function
 	model.compile(optimizer=RMSprop(lr=learning_rate), loss='binary_crossentropy')
 
 	model.summary()
@@ -123,12 +143,25 @@ def initialize_model(kernel_size, learning_rate):
 	return model
 
 def train_model(model, X_train, Y_train, X_test, Y_test, num_batches, num_epochs):
+	"""
+	Train the model
 
+	model: Compiled model
+	X_train, Y_train: Training data
+	X_test, Y_test: Validation data
+	num_batches: The batch size for training the network
+	num_epochs: The number of epochs to train the network
+
+	Return: Training and validation loss
+	"""
+
+	#Create a callback to output an image every 50 epochs
 	show_output = OutputObserver(50,model,X_train,Y_train)
 
 	history = model.fit(X_train, Y_train, validation_data=(X_test,Y_test), 
-			  batch_size=num_batches, epochs=num_epochs, callbacks=[show_output])
+			  batch_size=num_batches, epochs=num_epochs, callbacks=[show_output, TensorBoard(log_dir='./Graph')])
 
+	#Save the model
 	model.save('color_cnn.h5')
 
 	loss = history.history['loss']
@@ -139,6 +172,7 @@ def train_model(model, X_train, Y_train, X_test, Y_test, num_batches, num_epochs
 def main():
 	start = time()
 
+	#Path to data
 	path = '../../colorize_image_data.h5'
 
 	#Load in data
@@ -148,6 +182,7 @@ def main():
 	model = initialize_model(kernel_size=4, learning_rate=0.001)
 	loss, val_loss = train_model(model, X_train, Y_train, X_test, Y_test, num_batches=5, num_epochs=1501)
 
+	#Plot the losses
 	plt.plot(np.arange(len(loss)), loss, label='Training loss')
 	plt.plot(np.arange(len(val_loss)), val_loss, label='Validation loss')
 	plt.xlabel('Epoch')
@@ -159,7 +194,6 @@ def main():
 
 	end = time()-start
 	print('Time taken: {:.1f} s'.format(end))
-
 
 if __name__ == '__main__':
 	main()
